0e569f25bfdc7460498c42d8a62a3fa484ca6215
Tim Jacomb <timjacomb1@gmail.com>
2026-08-07 08:25:15 +0100

    Migrate from Commons Lang 2 to Commons Lang 3

--- src/main/java/com/itemis/jenkins/plugins/unleash/UnleashMavenBuildWrapper.java
+++ src/main/java/com/itemis/jenkins/plugins/unleash/UnleashMavenBuildWrapper.java
@@ -29,7 +29,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.model.Model;
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
